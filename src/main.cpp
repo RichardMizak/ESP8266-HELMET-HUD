3bc64ed2cd767f7be1ba7599ae5a5d311fafdbd1
Richard <richard.mizak@kosickaakademia.sk>
2023-03-12 23:41:50 +0100

    finished gps integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
-#include <NTPClient.h>
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
 #include <Adafruit_Sensor.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
@@ -17,9 +14,12 @@
 #define DHTPIN 12
 #define DHTTYPE DHT22
 
+int button = D1;
+int status = false;
+
 void display_left();
-void display_rigth();
-String NTPtime();
+void display_rigth_speed();
+void display_rigth_temp();
 
 DHT dht(DHTPIN, DHTTYPE);
 int read_temperature();
@@ -29,33 +29,17 @@ static const uint32_t GPSBaud = 9600;
 
 TinyGPSPlus gps;
 double get_speed();
-
+void get_time();
 SoftwareSerial ss(RXPin, TXPin);
 
-const char *ssid     = "Wavex";
-const char *password = "WaveX123";
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
-
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C display1(U8G2_MIRROR, /* reset=*/ U8X8_PIN_NONE, D1, D2);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C display1(U8G2_MIRROR, /* reset=*/ U8X8_PIN_NONE, D3, D7);
 U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C display2(U8G2_MIRROR, /* clock=*/ D4, /* data=*/ D5, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
   ss.begin(GPSBaud);
-  WiFi.begin(ssid, password);
-
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
-
-  timeClient.begin();
-
-  //u8g2_SetI2CAddress(&display1, 0x7A);
-  //u8g2_SetI2CAddress(&display2, 0x78);
+  pinMode(button, INPUT);
   display1.begin();
   display2.begin();
   display1.setFont(u8g2_font_inr30_mf);
@@ -67,9 +51,16 @@ void setup() {
 void loop() {
    while (ss.available() > 0)
     if (gps.encode(ss.read()))
-       display_left();
-       display_rigth();
-
+      if (digitalRead(button) == true){
+        status = !status;
+        if (status){
+          display_left();
+          display_rigth_speed();
+        }else{
+          display_left();
+          display_rigth_temp();
+        }
+      }   
   if (millis() > 5000 && gps.charsProcessed() < 10)
   {
     Serial.println(F("No GPS detected: check wiring."));
@@ -85,13 +76,25 @@ void display_left()
   do{
     display2.clearBuffer();
     display2.setCursor(0, 32);
-    display2.print(NTPtime());
+    get_time();
     display2.sendBuffer();
   }while (display2.nextPage());  
   delay(2000);
 }
 
-void display_rigth()
+void display_rigth_speed()
+{
+   display1.firstPage();
+  do{
+    display1.clearBuffer();
+    display1.setCursor(0, 32);
+    display1.print(get_speed());
+    display1.sendBuffer();
+  }while (display1.nextPage());  
+  delay(2000);
+}
+
+void display_rigth_temp()
 {
    display1.firstPage();
   do{
@@ -113,12 +116,15 @@ int read_temperature(){
   return temperature;
 }
 
-String NTPtime(){
-  timeClient.setTimeOffset(3600);
-  timeClient.update();
-  String x_time = timeClient.getFormattedTime();
-  delay(1000);
-  return x_time;
+void get_time(){
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) display2.print(F("0"));
+    display2.print(gps.time.hour() + 1);
+    display2.print(F(":"));
+    if (gps.time.minute() < 10) display2.print(F("0"));
+    display2.print(gps.time.minute());
+  }
 }
 
 double get_speed(){
